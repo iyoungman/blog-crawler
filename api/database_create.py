d170a053f95d6351185134ef2eb8d902a9b6a03e
iyoungman <hsl9546@gmail.com>
2019-12-20 02:42:19 +0900

    update

--- api/database_create.py
+++ api/database_create.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine(
-    'mysql+pymysql://root:duddnjsgl912!@iyoungman-skuniv-mysql-1.chxhgtrfuigt.ap-northeast-2.rds.amazonaws.com/blog_crawling?charset=utf8',
+    'mysql+pymysql://,
     convert_unicode=True, echo=True)
 
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
